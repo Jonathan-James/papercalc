5450b359f5ed18497fe9d482e0022f0ac4ed67aa
Jonathan James <jonathan@gmail.com>
2016-03-19 10:11:07 -0700

    initial commit

---
+++ color.js
@@ -0,0 +1,27 @@
+			
+
+
+
+			$(document).ready(function() {
+
+  					$(".red").click(function(){
+    				$("#calcbody").css("background-color", "#EF5350");
+    				$(".calc-wrapper").css("background-color", "#EF5350");
+});
+    				$(".green").click(function(){
+    				$("#calcbody").css("background-color", "#AED581");
+    				$(".calc-wrapper").css("background-color", "#AED581");
+});
+    				$(".blue").click(function(){
+    				$("#calcbody").css("background-color", "#4DD0E1");
+    				$(".calc-wrapper").css("background-color", "#4DD0E1");
+});
+    				$(".orange").click(function(){
+    				$("#calcbody").css("background-color", "#FFB74D");
+    				$(".calc-wrapper").css("background-color", "#FFB74D");
+});
+                    $(".purple").click(function(){
+                    $("#calcbody").css("background-color", "#9575CD");
+                    $(".calc-wrapper").css("background-color", "#9575CD");
+});
+});
---
+++ papercalc.js
@@ -0,0 +1,33 @@
+
+
+	var box = document.getElementById('display');
+
+		function addtoscreen(x){
+			box.value+=x;
+			if(x=='c'){
+				box.value='';
+			}
+		}
+
+		function removenumber(){
+			var number=box.value;
+			var len=number.length-1;
+			var newnumber=number.substring(0,len);
+			box.value=newnumber;
+		}
+
+
+		function answer(){
+			var x=box.value;
+			x=eval(x)
+			box.value=x;
+		}
+		
+		function power(y){
+			var x=box.value
+			var answer=eval(x);
+			var power=Math.pow(answer,y);
+			box.value=power;
+		}
+		
+		
